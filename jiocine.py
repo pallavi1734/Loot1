01ed106013df3a0e009995d3eaa6eebde2ea8801
aryanchy451 <pht449@gmail.com>
2025-01-06 16:33:53 +0530

    Update jiocine.py

--- jiocine.py
+++ jiocine.py
@@ -4,7 +4,7 @@ import xmltodict
 # Request object with Session maintained
 session = requests.Session()
 #session.proxies.update({'http': "http://bobprakash4646:ivR8gSbjLN@103.172.85.130:49155"})
-session.proxies.update({'http': "http://bobprakash4646:ivR8gSbjLN@103.172.85.130:49155"})
+session.proxies.update({'http': "http://toonrips:xipTsP9H9s@103.171.51.246:50100"})
 proxy = {'http':'http://toonrips:xipTsP9H9s@103.171.51.246:50100','https':"http://toonrips:xipTsP9H9s@103.171.51.246:50100"}
 # Common Headers for Session
 headers = {
